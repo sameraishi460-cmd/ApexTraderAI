14bc75605714420df705ceb054e4bffc409e87b9
sameraishi460-cmd <sameraishi460@gmail.com>
2026-07-19 21:07:53 +0300

    Integrate dynamic BTC price and update server port

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify
+from market import get_btc_price
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
@@ -10,12 +10,16 @@ def home():
 
 @app.route("/status")
 def status():
+    price = get_btc_price()
+
     return jsonify({
         "bot": "ONLINE",
-        "price": "65000",
-        "signal": "BUY"
+        "price": price,
+        "signal": "WAITING"
     })
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
