3a2ee93301991eaf0e99af211c6140a20bd612c9
sameraishi460-cmd <sameraishi460@gmail.com>
2026-07-19 18:33:33 +0300

    Enhance bot with signal calculation and risk management
    Added signal calculation and risk management features to the bot.

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 import time
+from strategy import calculate_signal, risk_management
+
+balance = 1000
+risk = 1
 
 def start_bot():
-    print("ApexTrader AI Bot Started")
+    print("🚀 ApexTrader AI Started")
 
     while True:
-        # هنا لاحقاً نضيف تحليل السوق والأوامر
-        print("Bot is running...")
+        # بيانات تجريبية مؤقتة
+        ema_fast = 105
+        ema_slow = 100
+        price = 105
+
+        signal = calculate_signal(
+            price,
+            ema_fast,
+            ema_slow
+        )
+
+        risk_amount = risk_management(
+            balance,
+            risk
+        )
+
+        print("Price:", price)
+        print("Signal:", signal)
+        print("Risk:", risk_amount)
+
         time.sleep(60)
 
 
